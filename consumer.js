9ca7c4e6d496d1d14e8251642f7bf599e1e74804
Kulum-eng <120421270+Kulum-eng@users.noreply.github.com>
2025-03-07 07:28:54 -0600

    firs

---
+++ consumer.js
@@ -0,0 +1,53 @@
+const amqplib = require("amqplib/callback_api");
+
+const urlServerRabbitMQ = "amqp://ale:ale123@ec2-3-83-91-51.compute-1.amazonaws.com/";
+const endpoint = "http://52.202.40.9:4000/api/Pagos";
+const queue = "cola1";
+
+amqplib.connect(urlServerRabbitMQ, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    channel.assertQueue(queue, {
+      durable: true,
+    });
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    
+    channel.consume(
+      queue,
+      async function (msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+         try {
+           const headers = {
+             "Content-Type": "application/json",
+           };
+           const req = {
+            method: "POST",
+             body: msg.content.toString(),
+             headers,
+           };
+           const result = await fetch(
+             endpoint,
+             req
+           );
+    
+           const data = await result.json();
+           console.log(data);
+           console.log(JSON.parse(msg.content.toString()));
+         } catch (error) {
+           console.log(error);
+         }
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
+});
